19f60b21c6ca58eb99fe6f851e3474de17773d3c
aydotjs <ayobami66@gmail.com>
2023-08-01 10:55:04 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import FriendList from "./components/FriendList.Js";
 
-function App() {
+const initialFriends = [
+  {
+    id: 118836,
+    name: "Boye",
+    image: "https://i.pravatar.cc/48?u=118836",
+    balance: -7,
+  },
+  {
+    id: 933372,
+    name: "Tayo",
+    image: "https://i.pravatar.cc/48?u=933372",
+    balance: 20,
+  },
+  {
+    id: 499476,
+    name: "Anthony",
+    image: "https://i.pravatar.cc/48?u=499476",
+    balance: 0,
+  },
+];
+
+export default function App() {
+  const [showAddFriendForm, setShowAddFriendForm] = useState(false);
+  function handleDisplayAddForm() {
+    setShowAddFriendForm((show) => !show);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="sidebar">
+        <FriendList />
+        {showAddFriendForm && <AddFriendForm />}
+        <Button onClick={handleDisplayAddForm}>
+          {showAddFriendForm ? "Close" : "Add Friend"}
+        </Button>
+      </div>
+      <FormSplitBill />
     </div>
   );
 }
 
-export default App;
+
+
+
+
+function AddFriendForm() {
+  return (
+    <form className="form-add-friend">
+      <label>🧑🏻‍❤️‍🧑🏽Add Friend</label>
+      <input type="text" />
+      <label>🖼️Image URL</label>
+      <input type="text" />
+    </form>
+  );
+}
+
+function FormSplitBill() {
+  return (
+    <form className="form-split-bill">
+      <h2>Split a bill with X</h2>
+      <label>💸Bill Value</label>
+      <input type="text" />
+      <label>🧑Your expense</label>
+      <input type="text" />
+      <label>🧑‍🦰Friend's expense</label>
+      <input type="text" disabled />
+      <label>🤔Whos's paying</label>
+      <select>
+        <option>You</option>
+        <option>Your friend</option>
+      </select>
+      <Button>Split Bill</Button>
+    </form>
+  );
+}
---
+++ src/components/Friends.js
@@ -0,0 +1,20 @@
+export default function Friends({ friend }) {
+    return (
+      <li>
+        <img src={friend.image} alt={friend.name} />
+        <h3>{friend.name}</h3>
+        {friend.balance > 0 && (
+          <p className="green">
+            {friend.name} owes you 💲{Math.abs(friend.balance)}
+          </p>
+        )}
+        {friend.balance < 0 && (
+          <p className="red">
+            you owe {friend.name} 💲{Math.abs(friend.balance)}
+          </p>
+        )}
+        {friend.balance === 0 && <p>you are even with {friend.name}</p>}
+        <Button>Select</Button>
+      </li>
+    );
+  }
